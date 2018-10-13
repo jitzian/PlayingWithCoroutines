92787565bca6b616121f5c386de58bfa8babb5af
raian <jitzian@gmail.com>
2018-10-12 23:52:00 -0400

    myTaxi jc - Cleanup un BaseClass

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
@@ -3,12 +3,14 @@ package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments
 import android.support.v4.app.Fragment
 import android.view.View
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.providers.RetrofitProvider
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.rest.RestService
 import retrofit2.Retrofit
 import java.util.logging.Logger
 
 abstract class BaseFragment: Fragment() {
-    lateinit var log: Logger
-    lateinit var rootview: View
-    var retrofit: Retrofit = RetrofitProvider.getInstance().providesRetrofit()
+    protected lateinit var log: Logger
+    protected lateinit var rootview: View
+    protected var retrofit: Retrofit = RetrofitProvider.getInstance().providesRetrofit()
+    protected lateinit var restService: RestService
     fun initView(){}
 }
