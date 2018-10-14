6a6d60af53dc438b7c9c6980d9e9745938340740
raian <jitzian@gmail.com>
2018-10-14 14:09:28 -0400

    myTaxi jc - Creating worker task for avoiding AsyncTask cuz I dont like them =)

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/db/threadManager/DbWorkerThread.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/db/threadManager/DbWorkerThread.kt
@@ -1,2 +1,18 @@
 package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.db.threadManager
 
+import android.os.Handler
+import android.os.HandlerThread
+
+class DbWorkerThread(threadName: String): HandlerThread(threadName){
+    private lateinit var mWorkerThreadHandler: Handler
+
+    override fun onLooperPrepared() {
+        super.onLooperPrepared()
+        mWorkerThreadHandler = Handler(looper)
+    }
+
+    fun postTask(task: Runnable){
+        mWorkerThreadHandler.post(task)
+    }
+
+}
