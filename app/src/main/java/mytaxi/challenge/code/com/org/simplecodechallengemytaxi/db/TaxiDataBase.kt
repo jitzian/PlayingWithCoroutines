0e44b2e891b74a995049bae6ac30aab36f75e9de
raian <jitzian@gmail.com>
2018-10-14 14:07:13 -0400

    myTaxi jc - Creating new Instance of RoomDB

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/db/TaxiDataBase.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/db/TaxiDataBase.kt
@@ -1,2 +1,33 @@
 package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.db
 
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.constants.GlobalConstants
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.db.dao.TaxiDao
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.db.model.Taxi
+
+@Database(entities = [Taxi::class], version = GlobalConstants.dataBaseVersion)
+abstract class TaxiDataBase: RoomDatabase(){
+
+    abstract fun taxiDao(): TaxiDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: TaxiDataBase? = null
+
+        fun getInstance(context: Context): TaxiDataBase?{
+            if(INSTANCE == null){
+                synchronized(TaxiDataBase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            TaxiDataBase::class.java, GlobalConstants.dataBaseName)
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+
+    }
+}
