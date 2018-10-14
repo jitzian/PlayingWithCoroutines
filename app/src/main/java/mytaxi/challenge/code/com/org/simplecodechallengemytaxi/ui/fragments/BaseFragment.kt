e57471db4749790b59bb97e147d52764b4d5c0dd
raian <jitzian@gmail.com>
2018-10-14 16:25:24 -0400

    myTaxi jc - Cleanup and removing variables that are no longer required.

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
@@ -3,18 +3,16 @@ package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments
 import android.support.v4.app.Fragment
 import android.view.View
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.components.DataComponent
-import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.providers.RetrofitProvider
-import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.rest.RestService
-import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.viewmodel.TaxiViewModel
-import retrofit2.Retrofit
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.rest.FetchDataService
 import java.util.logging.Logger
 
-abstract class BaseFragment: Fragment() {
+abstract class BaseFragment : Fragment() {
     protected lateinit var log: Logger
     protected lateinit var rootView: View
-    protected var retrofit: Retrofit = RetrofitProvider.getInstance().providesRetrofit()
-    protected lateinit var restService: RestService
-    protected lateinit var taxiViewModel: TaxiViewModel
     protected lateinit var dataComponent: DataComponent
     abstract fun initView()
+
+    protected lateinit var fetchDataService: FetchDataService
+    open fun fetchData() {}
+
 }
