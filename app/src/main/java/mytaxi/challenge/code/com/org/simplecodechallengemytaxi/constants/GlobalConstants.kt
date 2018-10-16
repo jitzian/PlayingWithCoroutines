e5c84605620d9a76524b993c3b1ec4852359d92d
Jonathan Cruz <jitzian@gmail.com>
2018-10-15 21:19:35 -0400

    myTaxi jc - Updating GlobalConstants

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/constants/GlobalConstants.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/constants/GlobalConstants.kt
@@ -5,5 +5,6 @@ class GlobalConstants{
         const val baseURL = "https://fake-poi-api.mytaxi.com"
         const val dataBaseName = "taxi_database.db"
         const val dataBaseVersion = 1
+        const val textSnakBar = "Moving to selected position"
     }
 }
