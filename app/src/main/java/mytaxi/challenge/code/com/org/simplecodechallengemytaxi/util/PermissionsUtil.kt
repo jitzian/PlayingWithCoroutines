8b1e38b563a3589b51b64127c0cbab90a1c0fbc3
Jonathan Cruz <jitzian@gmail.com>
2018-10-16 19:53:23 -0400

    myTaxi jc - Creating basic component for checking permissions

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/util/PermissionsUtil.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/util/PermissionsUtil.kt
@@ -1,2 +1,26 @@
 package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.util
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+
+class PermissionsUtil(val context: Context?, private val activityCompat: Activity?) {
+
+    fun requestPermissions() {
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        activityCompat?.let {
+            ActivityCompat.requestPermissions(it, permissions, 0)
+        }
+    }
+
+    private fun isPermissionGranted(permission: String): Boolean =
+            context?.let {
+                ContextCompat.checkSelfPermission(
+                        it,
+                        permission
+                )
+            } == PackageManager.PERMISSION_GRANTED
+}
