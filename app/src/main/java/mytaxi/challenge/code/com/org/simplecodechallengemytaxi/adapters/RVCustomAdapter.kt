db9e091d9ca8c294e4d4cef73755b0ee06ace23f
Jonathan Cruz <jitzian@gmail.com>
2018-10-16 19:00:23 -0400

    myTaxi jc - Changing style on cardview + Adapter

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/adapters/RVCustomAdapter.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/adapters/RVCustomAdapter.kt
@@ -1,5 +1,5 @@
 package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.adapters
-
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
@@ -17,18 +17,16 @@ import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.constants.GlobalC
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.constants.GlobalConstants.Companion.typeTaxi
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.model.PoiList
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments.MapsFragment
-
 class RVCustomAdapter(private var lstRes: List<PoiList>?, private val context: Context, private var fragmentManager: FragmentManager?) : RecyclerView.Adapter<RVCustomAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.card_view_taxi, p0, false))
     }
-
     override fun getItemCount(): Int {
         lstRes.let {
             return it?.size!!
         }
     }
-
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         when (lstRes?.get(position)?.fleetType) {
             typePool -> {
@@ -39,33 +37,30 @@ class RVCustomAdapter(private var lstRes: List<PoiList>?, private val context: C
             }
         }
         holder.mTextViewFleetTypeValue.text = lstRes?.get(position)?.fleetType
+        holder.mTextViewFromValueLatitude.text = lstRes?.get(position)?.coordinate?.latitude.toString()
+        holder.mTextViewFromValueLongitude.text = lstRes?.get(position)?.coordinate?.longitude.toString()
     }
-
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         lateinit var mImageViewTaxi: ImageView
-        lateinit var mTextViewFleetType: TextView
         lateinit var mTextViewFleetTypeValue: TextView
-
+        lateinit var mTextViewFromValueLatitude: TextView
+        lateinit var mTextViewFromValueLongitude: TextView
         init {
             itemView.let {
                 mImageViewTaxi = it.findViewById(R.id.mImageViewTaxi)
-                mTextViewFleetType = it.findViewById(R.id.mTextViewFleetType)
                 mTextViewFleetTypeValue = it.findViewById(R.id.mTextViewFleetTypeValue)
-
+                mTextViewFromValueLatitude = it.findViewById(R.id.mTextViewFromValueLatitude)
+                mTextViewFromValueLongitude = it.findViewById(R.id.mTextViewFromValueLongitude)
                 itemView.setOnClickListener { v: View ->
                     val position: Int = adapterPosition
-
                     Snackbar.make(v, "${GlobalConstants.textSnackBar}:: " +
                             "${lstRes?.get(position)?.coordinate?.latitude.toString()}, ${lstRes?.get(position)?.coordinate?.longitude.toString()}",
                             Snackbar.LENGTH_LONG).setAction("Action", null).show()
                     val gotoMapsFragment = MapsFragment()
                     val bundle = Bundle()
-
                     bundle.putString("latitude", lstRes?.get(position)?.coordinate?.latitude.toString())
                     bundle.putString("longitude", lstRes?.get(position)?.coordinate?.longitude.toString())
-
                     gotoMapsFragment.arguments = bundle
-
                     Handler().postDelayed({
                         fragmentManager?.beginTransaction()?.replace(R.id.mFrameLayoutMainContainer, gotoMapsFragment, MapsFragment::class.java.simpleName)?.commit()
                     }, 500)
@@ -73,5 +68,4 @@ class RVCustomAdapter(private var lstRes: List<PoiList>?, private val context: C
             }
         }
     }
-
 }
