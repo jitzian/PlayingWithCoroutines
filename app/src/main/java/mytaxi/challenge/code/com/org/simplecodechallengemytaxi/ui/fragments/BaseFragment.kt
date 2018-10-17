f73b6818741b34fec44a5692a8aacf5b3c11c521
Jonathan Cruz <jitzian@gmail.com>
2018-10-16 20:04:56 -0400

    myTaxi jc - Just adding some documentation

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
@@ -3,14 +3,21 @@ package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments
 import android.support.v4.app.Fragment
 import android.view.View
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.rest.FetchDataService
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.util.PermissionsUtil
 import java.util.logging.Logger
+
+/**
+ * Baseclass that encapsulates base needed components
+ * **/
 
 abstract class BaseFragment : Fragment() {
     protected lateinit var log: Logger
     protected lateinit var rootView: View
     abstract fun initView()
 
+    protected lateinit var permissionsUtil: PermissionsUtil
     protected lateinit var fetchDataService: FetchDataService
+
     open fun fetchData() {}
 
     //This function is to manage 2 parameters with let, so, I'm avoiding var1.let{ it.var2.let{...
