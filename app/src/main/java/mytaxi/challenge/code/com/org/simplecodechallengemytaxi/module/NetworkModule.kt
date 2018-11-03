a48d9acb6811fe45bc82dff59620e6ad5bc6a5bb
Jonathan Cruz <jitzian@gmail.com>
2018-11-02 21:35:16 -0400

    myTaxi jc - Reorganizing dependencies

---
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/module/NetworkModule.kt
@@ -0,0 +1,22 @@
+package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.module
+
+import dagger.Module
+import dagger.Provides
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.constants.GlobalConstants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.Executors
+
+@Module
+class NetworkProvider {
+
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(GlobalConstants.baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .callbackExecutor(Executors.newSingleThreadExecutor())
+                .build()
+    }
+
+}
