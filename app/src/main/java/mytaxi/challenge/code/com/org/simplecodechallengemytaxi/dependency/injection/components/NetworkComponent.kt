915e22e331429676ef99a02103d10d4351a5295a
Jonathan Cruz <jitzian@gmail.com>
2018-11-03 22:03:00 -0400

    myTaxi jc - Cleanup of Dagger NetworkComponent

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/dependency/injection/components/NetworkComponent.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/dependency/injection/components/NetworkComponent.kt
@@ -1,12 +1,13 @@
-package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.dependency.injection.module.components
+package mytaxi.challenge.code.com.org.simplecodechallengemytaxi.dependency.injection.components
 
 import dagger.Component
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.dependency.injection.module.NetworkModule
+import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.presenter.ListTaxiPresenterImpl
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments.ListMyTaxiFragment
 import mytaxi.challenge.code.com.org.simplecodechallengemytaxi.ui.fragments.MapsFragment
 
 @Component(modules = [NetworkModule::class])
 interface NetworkComponent {
-    fun inject(listMyTaxiFragment: ListMyTaxiFragment)
     fun inject(mapsFragment: MapsFragment)
+    fun inject(listTaxiPresenter: ListTaxiPresenterImpl)
 }
