f5ebec473966fc0eafcc1326a2e54554d9e1ac3a
Jonathan Cruz <jitzian@gmail.com>
2018-11-03 22:02:22 -0400

    myTaxi jc - Cleanup of BaseFragment. Still needs work after refactoring MapsFragment.

--- app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
+++ app/src/main/java/mytaxi/challenge/code/com/org/simplecodechallengemytaxi/ui/fragments/BaseFragment.kt
@@ -19,11 +19,6 @@ abstract class BaseFragment : Fragment() {
 
     open fun fetchData() {}
 
-    protected lateinit var restService: RestService
-
-    protected fun checkDataInRoomDataBase(){}
-
-    //This function is to manage 2 parameters with let, so, I'm avoiding var1.let{ it.var2.let{...
     fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
         return if (p1 != null && p2 != null) block(p1, p2) else null
     }
